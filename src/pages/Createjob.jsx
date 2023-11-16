c9671045c91af78ee8fa9075926e72e38ade2922
Milanpraz <mln.praz12@gmail.com>
2023-11-16 13:08:04 +0545

    updated errors

--- src/pages/Createjob.jsx
+++ src/pages/Createjob.jsx
@@ -160,7 +160,7 @@ function Createjob() {
                   <input
                     onChange={(e) => handleChange(e)}
                     value={jobData.location}
-                    name="experience"
+                    name="location"
                     type="text"
                     className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset focus:outline-none ring-gray-400 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6"
                   />
